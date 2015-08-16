4134f8bd4408d1446d7c981aaf647d5b5bae258a
Andr <andxbes@gmail.com>
2015-08-16 14:20:56 +0300

    archetupe PHPMVC

---
+++ classes/Controller_Base.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Description of Controller_Base
+ *
+ * @author Andr
+ */
+abstract class Controller_Base {
+    protected $registry;
+    protected $template;
+    protected $layouts; // шаблон
+     
+    public $vars = array();
+ 
+    // в конструкторе подключаем шаблоны
+    function __construct($registry) {
+        $this->registry = $registry;
+        // шаблоны
+        $this->template = new Template($this->layouts, get_class($this));
+    }
+ 
+    abstract function index(); 
+}
