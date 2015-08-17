9159c6115077a27c3ac7bf22f08f6a2c196139ac
Andr <andxbes@gmail.com>
2015-08-17 19:51:02 +0300

    add to first-layouts.php the  line "charset"

--- app/views/layouts/first_layouts.php
+++ app/views/layouts/first_layouts.php
@@ -1,12 +1,17 @@
 <! DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="http://<?=$_SERVER['HTTP_HOST']?>/resources/css/style.css">
+        <link type="text/javascript" href="http://<?=$_SERVER['HTTP_HOST']?>/resources/js/script.js" >
+        
     </head>        
     <body>
                 
         <h1> header </h1>
+        <h3>Привет мир </h3>
             <?php
+            
             include ($contentPage);
             ?>
         <h1> footer </h1>   
